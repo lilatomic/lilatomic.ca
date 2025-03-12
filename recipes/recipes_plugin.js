fa9028c91b5f1db5fadfe2606a681fae6cb7cab4
Daniel Goldman <danielgoldman4@gmail.com>
2025-03-12 17:21:19 -0400

    improve formatting of ingredients

--- recipes/recipes_plugin.js
+++ recipes/recipes_plugin.js
@@ -1,14 +1,14 @@
-// const { JSDOM } = require("jsdom");
 const util = require('util')
 const {Recipe, Operation, Ingredient} = require('./models')
 
 class Cell {
-	constructor(content, x, y, width, height, children = []) {
+	constructor(content, x, y, width, height, style, children = []) {
 		this.content = content;
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		this.style = style;
 		this.children = children;
 	}
 }
@@ -47,12 +47,12 @@ function getContent(item) {
 function createCellRepresentation(it, item, parent_xpos) {
 	const xPosition = getMaxDepth(item);
 	if (item instanceof Ingredient) {
-		return new Cell(getContent(item), xPosition, it.increment(), parent_xpos - xPosition, 1);
+		return new Cell(getContent(item), xPosition, it.increment(), parent_xpos - xPosition, 1, "recipe_ingredient");
 	} else if (item instanceof Operation) {
 		const children = item.dependencies.map(e => createCellRepresentation(it, e, xPosition));
 		const yPosition = Math.min(...children.map(e => e.y));
 		const height = children.reduce((sum, child) => sum + child.height, 0);
-		return new Cell(getContent(item), xPosition, yPosition, parent_xpos - xPosition, height, children);
+		return new Cell(getContent(item), xPosition, yPosition, parent_xpos - xPosition, height, "recipe_operation", children);
 	}
 }
 
@@ -69,7 +69,7 @@ function _compare_cells(a, b) {
 }
 
 function generateTable(root) {
-	const flattened = _flatten(root).map(e => new Cell(e.content, e.x, e.y, e.width, e.height));
+	const flattened = _flatten(root).map(e => new Cell(e.content, e.x, e.y, e.width, e.height, e.style));
 	const sorted = flattened.sort(_compare_cells);
 
 	const tableRows = [];
@@ -77,14 +77,12 @@ function generateTable(root) {
 		while (tableRows.length <= cell.y) {
 			tableRows.push([]);
 		}
-		tableRows[cell.y].push(`<td colspan="${cell.width}" rowspan="${cell.height}">${cell.content}</td>`);
+		tableRows[cell.y].push(`<td colspan="${cell.width}" rowspan="${cell.height}" class="${cell.style}">${cell.content}</td>`);
 	}
 
-	const tableHTML = `<table>
+	return `<table>
 		${tableRows.map(row => `<tr>${row.join('')}</tr>`).join('\n')}
 	</table>`;
-
-	return tableHTML;
 }
 
 function renderRecipe(recipe) {

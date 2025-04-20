43d1eb3a08002e0cc7c47c591e9dedb6cc8cadf4
Daniel Goldman <danielgoldman4@gmail.com>
2025-04-20 13:14:56 -0400

    fix nav selected item

--- _includes/components/Nav.tsx
+++ _includes/components/Nav.tsx
@@ -5,15 +5,24 @@ export type ThisNav = {
 export function Nav(this: ThisNav) {
 	const f = this.context.s.functions;
 	const data = this.context.data;
+
+	function li_class(entry) {
+		if (entry.url === data.page.url) {
+			return "nav-item nav-item-active";
+		} else {
+			return "nav-item";
+		}
+	}
+
 	return (
 		<nav class="nav">
 			<ul>
 				{
-					f.eleventyNavigation(data.collections.all).map(entry => {
-						<li class={`nav-item ${entry.url === data.page.url && "nav-item-active"}`}>
+					f.eleventyNavigation(data.collections.all).map(entry =>
+						<li class={li_class(entry)}>
 							<a href={f.url(entry.url)}>{entry.title}</a>
 						</li>
-					})
+					)
 				}
 			</ul>
 		</nav>
